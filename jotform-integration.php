0c3ee6813f4fd0a7acde4a2d8520d59f8f086020
Patrick Rauland <bftrick@gmail.com>
2012-06-20 14:06:48 -0500

    added options panel

--- jotform-integration.php
+++ jotform-integration.php
@@ -5,7 +5,7 @@ Plugin URI: https://github.com/BFTrick/jotform-integration
 Description: Integrate JotForm forms with your WordPress-powered site.
 Author: Patrick Rauland
 Author URI: http://www.patrickrauland.com
-Version: 1.0
+Version: 1.0.1
 License: GPL2
 */
 
